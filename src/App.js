8f3b0e26e88f95eabf65d02bdf5a5bbe22167278
chdineshbabu <chdinesh4128@gmail.com>
2023-10-12 10:47:50 +0530

    initail 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Login from './Components/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar />
     <Routes>
+    <Route path='/login' element={<Login />} />
     </Routes>
   </BrowserRouter>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 function Navbar() {
+    const pathee = useParams()
+    console.log(pathee)
   return (
-    <div className='text-2xl md:text-7xl'>Navbar</div>
+    <div className='t-0'>
+        <div className='h-20 text-white bg-black'>
+        {pathee === '/' ? <h1>Faculty Leave Management</h1>:<div></div>}
+        </div>
+    </div>
   )
 }
 
