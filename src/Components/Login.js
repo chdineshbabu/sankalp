89999d2cad64b69fdf76646449c2399d38b6c91f
Abhishek <108579783+Abhishek6543jh@users.noreply.github.com>
2023-10-12 10:41:35 +0530

    h

---
+++ src/Components/Login.js
@@ -0,0 +1,39 @@
+// src/Login.js
+import React, { useState } from 'react';
+import { auth } from './firebase';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // Login successful, you can redirect or update UI here
+    } catch (error) {
+      // Handle login errors
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
