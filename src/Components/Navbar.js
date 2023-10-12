2d9e846edee2a217554826612f671bf306a5ad7f
chdineshbabu <chdinesh4128@gmail.com>
2023-10-12 10:57:37 +0530

    mibor xhne

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { useParams } from 'react-router'
 
 function Navbar() {
-    const pathee = useParams()
-    console.log(pathee)
   return (
     <div className='t-0'>
         <div className='h-20 text-white bg-black'>
-        {pathee === '/' ? <h1>Faculty Leave Management</h1>:<div></div>}
+        
         </div>
     </div>
   )
